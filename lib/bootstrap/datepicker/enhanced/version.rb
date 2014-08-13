1a437abf137cad2acc4af42d552552611daf81d1
Wolfgang Uhl <wolfgang.uhl@googlemail.com>
2014-08-13 17:05:50 +0200

    Update data fields in form.html

--- lib/bootstrap/datepicker/enhanced/version.rb
+++ lib/bootstrap/datepicker/enhanced/version.rb
@@ -1,7 +1,7 @@
 module Bootstrap
   module Datepicker
     module Enhanced
-      VERSION = "0.0.3"
+      VERSION = "0.0.4"
     end
   end
 end
--- lib/generators/bootstrapdatepickerenhanced/install/install_generator.rb
+++ lib/generators/bootstrapdatepickerenhanced/install/install_generator.rb
@@ -9,18 +9,18 @@ module Bootstrapdatepickerenhanced
       argument :language_type, :type => :string, :default => 'de', :banner => '*de or other language'
 
       def add_javascripts
-      	insert_into_file "app/assets/javascripts/application.js", :after => "//= require jquery_ujs\n" do 
+      	insert_into_file "app/assets/javascripts/application.js", :after => "//= require jquery_ujs\n" do
       		"//= require jquery.ui.datepicker\n" +
 					"//= require jquery.ui.datepicker-de\n" +
 					"//= require jquery.ui.datepicker-en-GB\n"
 				end
-      	insert_into_file "app/assets/javascripts/application.js", :after => "//= require twitter/bootstrap\n" do 
+      	insert_into_file "app/assets/javascripts/application.js", :after => "//= require twitter/bootstrap\n" do
       		"//= require bootstrap-datepicker\n"
 				end
       end
 
       def add_stylesheets
-      	insert_into_file "app/assets/stylesheets/application.css", :after => " *= require_self\n" do 
+      	insert_into_file "app/assets/stylesheets/application.css", :after => " *= require_self\n" do
       		" *= require bootstrap-datepicker\n"
 				end
       end
@@ -30,6 +30,18 @@ module Bootstrapdatepickerenhanced
       	copy_file "date_picker_input.rb", "app/inputs/date_picker_input.rb"
       	copy_file "datepicker.js.coffee", "app/assets/javascripts/datepicker.js.coffee"
       end
+
+			def adjust_forms_file
+        if File.exist? "lib/templates/#{engine}/scaffold/_form.html.#{engine}" and File.exist? "app/inputs/date_picker_input.rb"
+          gsub_file "lib/templates/#{engine}/scaffold/_form.html.#{engine}",
+					  "<%%= f.<%= attribute.field_type %> :<%= attribute.name %> %>",
+						"<%- if attribute.type.in?([:date]) -%>\n" +
+            "      <%%= f.text_field :<%= attribute.name %>, :input_html => { :size => 10, :maxlength => 10, :language => I18n.locale }, as: :date_picker %>\n" +
+            "      <%- else -%>\n" +
+      			"      <%%= f.<%= attribute.field_type %> :<%= attribute.name %> %>\n" +
+            "      <%- end -%>"
+				end
+			end
 
     end
   end
